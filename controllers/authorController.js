1f2c56adb6a5e02d7fd7c2f9843b5595f0a884f4
Himesh9512 <himeshjangid0000@gmail.com>
2023-08-06 18:54:51 +0530

    Create detail view for all models in app

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 const Author = require("../models/author");
+const Book = require("../models/book");
+
 const asyncHandler = require("express-async-handler");
 
 exports.author_list = asyncHandler(async (req, res, next) => {
@@ -10,7 +12,23 @@ exports.author_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.author_detail = asyncHandler(async (req, res, next) => {
-	res.send(`NOT IMPLEMENTED: Author list: ${req.params.id} `);
+	const [author, allBooksByAuthor] = await Promise.all([
+		Author.findById(req.params.id).exec(),
+		Book.find({ author: req.params.id }, "title summary").exec(),
+	]);
+
+	if (author === null) {
+		// No results.
+		const err = new Error("Author not found");
+		err.status = 404;
+		return next(err);
+	}
+
+	res.render("author_detail", {
+		title: "Author Detail",
+		author: author,
+		author_books: allBooksByAuthor,
+	});
 });
 
 exports.author_create_get = asyncHandler(async (req, res, next) => {
--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -11,7 +11,19 @@ exports.bookinstance_list = asyncHandler(async (req, res, next) => {
 });
 
 exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
-	res.send(`NOT IMPLEMENTED: BookInstance list: ${req.params.id} `);
+	const bookInstance = await BookInstance.findById(req.params.id).populate("book").exec();
+
+	if (bookInstance === null) {
+		// No results.
+		const err = new Error("Book copy not found");
+		err.status = 404;
+		return next(err);
+	}
+
+	res.render("bookinstance_detail", {
+		title: "Book:",
+		bookinstance: bookInstance,
+	});
 });
 
 exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
