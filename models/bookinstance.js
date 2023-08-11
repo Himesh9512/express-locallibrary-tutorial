42f2a41ecd39c2fe0ea5b45a10298199c7e05d58
Himesh9512 <himeshjangid0000@gmail.com>
2023-08-11 21:08:06 +0530

    Create book instance form

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -23,4 +23,7 @@ BookInstanceSchema.virtual("due_back_formatted").get(function () {
 	return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.Date_MED);
 });
 
+BookInstanceSchema.virtual("due_back_yyyy_mm_dd").get(function () {
+	return DateTime.fromJSDate(this.due_back).toISODate();
+});
 module.exports = mongoose.model("BookInstance", BookInstanceSchema);
